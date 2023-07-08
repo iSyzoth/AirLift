618e813516379a84296b96a75394bebb8141e6b8
sumitnce1 <skumaronline1@gmail.com>
2023-07-08 17:21:52 +0530

    Added AirLift-server

---
+++ AirLift-Server/src/test/java/com/savya/airliftserver/AirLiftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savya.airliftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirLiftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
