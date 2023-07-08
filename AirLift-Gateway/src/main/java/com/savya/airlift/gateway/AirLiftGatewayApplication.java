2b619e44c7e4c79e1edad8bf1fca1e77f3b013a0
sumitnce1 <skumaronline1@gmail.com>
2023-07-08 17:16:19 +0530

    Added AirLift-Gateway

---
+++ AirLift-Gateway/src/main/java/com/savya/airlift/gateway/AirLiftGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.savya.airlift.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLiftGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLiftGatewayApplication.class, args);
+	}
+
+}
---
+++ AirLift-Gateway/src/test/java/com/savya/airlift/gateway/AirLiftGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savya.airlift.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirLiftGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
