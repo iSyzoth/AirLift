7f53b5a4e9beaafd37c8295f2e8beb5b0f4bae10
sumitnce1 <skumaronline1@gmail.com>
2023-07-08 17:08:32 +0530

    Added AirLift Services

---
+++ AirLift/src/main/java/com/savya/airlift/AirLiftApplication.java
@@ -0,0 +1,13 @@
+package com.savya.airlift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLiftApplication.class, args);
+	}
+
+}
---
+++ AirLift/src/test/java/com/savya/airlift/AirLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savya.airlift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirLiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
